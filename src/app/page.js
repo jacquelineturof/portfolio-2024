1ba611a79a794e98bc2694b0cf4e5447af743deb
jacquelineturof <45600873+jacquelineturof@users.noreply.github.com>
2024-02-27 11:45:59 -0500

    set up initial page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,98 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+import { Open_Sans, Poiret_One } from "next/font/google";
+
+import Cursor from "../components/cursor";
+import Navbar from "../components/navbar";
+
+import { IconContext } from "react-icons";
+import {
+  IoLogoLinkedin,
+  IoLogoGithub,
+  IoMailOpenOutline,
+} from "react-icons/io5";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+const poiretOne = Poiret_One({
+  subsets: ["latin"],
+  weight: "400"
+});
 
 export default function Home() {
+  const [color, setColor] = useState("black");
+  const [isMouseOverNav, setIsMouseOverNav] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`h-screen w-screen p-8 ${openSans.className} flex flex-col bg-bg-gray`}>
+      <Cursor color={color} isNav={isMouseOverNav} />
+      <nav className="flex h-16 w-full items-center justify-between">
+        <div className={`flex flex-col text-3xl text-black leading-none ${poiretOne.className}`}>
+          <span>Jackie</span>
+          <span className="ml-4"> Turof </span>
         </div>
-      </div>
+        <Navbar setIsNav={setIsMouseOverNav} />
+        <div className="avatar">
+          <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src="/headshot.png" />
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      </nav>
+      <section className="flex-1 w-full flex flex-col mt-36">
+        <div
+          className="flex flex-col uppercase"
+          onMouseEnter={() => setColor("blue")}
+          onMouseLeave={() => setColor("black")}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <h1 className="text-black text-9xl font-bold">
+            Software
+          </h1>
+          <h1 className="text-black text-9xl font-bold ml-12">
+            Developer
+          </h1>
+        </div>
+        <div className="mt-auto flex justify-between">
+          <div className="flex flex-col">
+            <IconContext.Provider value={{
+              color: 'black',
+              size: 36,
+              style: {
+                marginBottom: '1rem'
+              }
+            }}>
+              <IoLogoLinkedin />
+              <IoLogoGithub />
+              <IoMailOpenOutline />
+            </IconContext.Provider>
+          </div>
+          <button
+            className="btn rounded-full h-24 w-24 mt-auto flex flex-col text-white"
+            onClick={() => document.getElementById('my_modal_1').showModal()}
+          >
+            <span className="font-bold">Stack</span>
+            <span className="text-xs font-light">&</span>
+            <span className="font-bold">CV</span>
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">My current stack</h3>
+              <p className="py-4">
+                My current tech stack is serverless. Utilizing NextJS with Typescript (and Redux), Supabase and Vercel.
+                I am currently studying for my AWS certification.
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+                <button className="btn btn-secondary">Download CV</button>
+              </div>
+            </div>
+          </dialog>
+        </div>
+      </section>
     </main>
   );
 }
