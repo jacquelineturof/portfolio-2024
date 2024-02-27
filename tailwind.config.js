5f181331edb584acde0463a0e4eb238286729553
jacquelineturof <45600873+jacquelineturof@users.noreply.github.com>
2024-02-27 11:44:22 -0500

    add daisy ui and some test colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  themes: ["light"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +13,18 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'purple-dark': '#AD7EC8',
+        'purple-light': '#F6F1F9',
+        'blue-dark': '#1F6C98',
+        'blue-light': '#EEF7FC',
+        'green-dark': '#279057',
+        'green-light': '#EFFBF4',
+        'green-light-1': '#F3F7F4',
+        'green-dark-1': '#78A584',
+        'bg-gray': '#EFEFFB'
+      }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
