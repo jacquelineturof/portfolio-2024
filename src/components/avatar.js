e4008f5eee4ca0c9bd857b05985d74a0a1bdfbad
jacquelineturof <45600873+jacquelineturof@users.noreply.github.com>
2024-02-28 15:00:02 -0500

    create avatar and menu components to use in responsive design

---
+++ src/components/avatar.js
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+    <div className="avatar">
+      <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="/headshot.png" />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/menu.js
@@ -0,0 +1,5 @@
+import { IoMenuOutline } from "react-icons/io5";
+
+export default function Menu() {
+  return <IoMenuOutline />;
+}
