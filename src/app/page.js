908c4907ff816aa265c97913438ede49ed0cf32f
jacquelineturof <45600873+jacquelineturof@users.noreply.github.com>
2024-02-28 13:59:59 -0500

    add new components

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,8 @@ import { Open_Sans, Poiret_One } from "next/font/google";
 
 import Cursor from "../components/cursor";
 import Navbar from "../components/navbar";
-
-import { IconContext } from "react-icons";
-import {
-  IoLogoLinkedin,
-  IoLogoGithub,
-  IoMailOpenOutline,
-} from "react-icons/io5";
+import StackButton from "@/components/stackButton";
+import SocialMediaMenu from "@/components/socailMediaMenu";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 const poiretOne = Poiret_One({
@@ -54,43 +49,8 @@ export default function Home() {
           </h1>
         </div>
         <div className="mt-auto flex justify-between">
-          <div className="flex flex-col">
-            <IconContext.Provider value={{
-              color: 'black',
-              size: 36,
-              style: {
-                marginBottom: '1rem'
-              }
-            }}>
-              <IoLogoLinkedin />
-              <IoLogoGithub />
-              <IoMailOpenOutline />
-            </IconContext.Provider>
-          </div>
-          <button
-            className="btn rounded-full h-24 w-24 mt-auto flex flex-col text-white"
-            onClick={() => document.getElementById('my_modal_1').showModal()}
-          >
-            <span className="font-bold">Stack</span>
-            <span className="text-xs font-light">&</span>
-            <span className="font-bold">CV</span>
-          </button>
-          <dialog id="my_modal_1" className="modal">
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">My current stack</h3>
-              <p className="py-4">
-                My current tech stack is serverless. Utilizing NextJS with Typescript (and Redux), Supabase and Vercel.
-                I am currently studying for my AWS certification.
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn">Close</button>
-                </form>
-                <button className="btn btn-secondary">Download CV</button>
-              </div>
-            </div>
-          </dialog>
+          <SocialMediaMenu />
+          <StackButton />
         </div>
       </section>
     </main>
