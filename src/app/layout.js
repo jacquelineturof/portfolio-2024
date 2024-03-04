8f8dc4f361bb058ccf17c769475111e7bef0b9eb
jacquelineturof <45600873+jacquelineturof@users.noreply.github.com>
2024-03-04 12:21:20 -0500

    use the new layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import Layout from "@/components/Layout";
 
 export const metadata = {
   title: "Jackie Software Developer",
@@ -11,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Layout>
+          {children}
+        </Layout>
+      </body>
     </html>
   );
 }
